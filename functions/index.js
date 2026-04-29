8e37d20742e9b023cf0e1715405e9ec9539dab1d
FireCodingdev <firecodingdev@gmail.com>
2026-04-29 17:50:52 -0300

    ajustes

--- functions/index.js
+++ functions/index.js
@@ -14,7 +14,7 @@ initializeApp();
 // A chave é armazenada como secret do Firebase (criptografada, nunca exposta)
 const geminiKey = defineSecret('GEMINI_API_KEY');
 
-const GEMINI_MODEL = 'gemini-2.0-flash';
+const GEMINI_MODEL = 'gemini-1.5-flash';
 const GEMINI_URL   = `https://generativelanguage.googleapis.com/v1beta/models/${GEMINI_MODEL}:generateContent`;
 
 exports.geminiProxy = onRequest(
